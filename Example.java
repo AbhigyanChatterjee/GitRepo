b7e66934dab83686e32bbf209fec174078126051
Abhigyan Chatterjee <abhigyan1042@gmail.com>
2020-06-05 17:10:33 +0530

    Second commit in Dev Branch

--- Example.java
+++ Example.java
@@ -6,6 +6,9 @@ public class Main {
 			res *= a;
 		}
 		return res;
+	}
+	static int mult(int a, int b) {
+		return (a * b);
 	}	
 	public static void main {
 		System.out.println("Abhigyan is the best");
@@ -13,6 +16,7 @@ public class Main {
 		int b = 14;
 		System.out.println((a + b)/(2*a - b));
 		System.out.println(power(a,b));
+		System.out.println(mult(a, b));
 		
 	}
 } 
