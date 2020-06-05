66a0bf6b2a98e7e668a17225f91a17b565a1fb27
Abhigyan Chatterjee <abhigyan1042@gmail.com>
2020-06-05 16:42:27 +0530

    Second commit-changes in example.java

--- Example.java
+++ Example.java
@@ -2,5 +2,8 @@ package abhi;
 public class Main {
 	public static void main {
 		System.out.println("Abhigyan is the best");
+		int a = 10;
+		int b = 14;
+		System.out.println((a + b)/(2*a - b));
 	}
 } 
