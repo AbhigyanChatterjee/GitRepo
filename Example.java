9cc14ccfb3a7dccbcdd5cc7d5ed571de8edcacf2
Abhigyan Chatterjee <abhigyan1042@gmail.com>
2020-06-05 16:53:18 +0530

    Example commit Dev branch

--- Example.java
+++ Example.java
@@ -1,9 +1,18 @@
 package abhi;
 public class Main {
+	static int power(int a, int b) {
+		int res = 1;
+		for(int i = 0; i < b; i++) {
+			res *= a;
+		}
+		return res;
+	}	
 	public static void main {
 		System.out.println("Abhigyan is the best");
 		int a = 10;
 		int b = 14;
 		System.out.println((a + b)/(2*a - b));
+		System.out.println(power(a,b));
+		
 	}
 } 
